97a24482d058c3a00af60b177d3690056ea22dfb
Sergey Pozniakevich <ven07@yandex.ru>
2022-10-27 01:15:32 +0700

    add complection

---
+++ completion_guide.py
@@ -0,0 +1,13 @@
+from guide_loggining import write_log_guide as wlg
+from guide_loggining import write_log_guide_txt as wlgt
+from input_guide import input_name, input_fam, input_description, input_number
+
+
+def complection():
+    a = input_fam()
+    b = input_name()
+    c = input_number()
+    d = input_description()
+    print(f'фамилия: {a} имя: {b} телефон: {c} описание: {d}')
+    wlg(a, b, c, d)
+    wlgt(a, b, c, d)
